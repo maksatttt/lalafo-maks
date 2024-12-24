ad6b4940b2a1f6f78a3ea7eec3d945e657042166
Maksat Akzholtoev <maks@Maksats-MacBook-Pro.local>
2024-12-24 13:43:16 +0600

    макс

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: 'https://lalafo.kg', // URL тестируемого сайта
+        setupNodeEvents(on, config) {
+            require('cypress-mochawesome-reporter/plugin')(on);
+        },
     },
-  },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+        reportDir: 'reports', // Папка для отчетов
+        overwrite: false,
+        html: true,
+        json: false,
+    },
 });
